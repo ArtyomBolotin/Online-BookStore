0fe4d0db2f834910088ae42c508d999a5a2af3b6
ArtyomBolotin <101791299+ArtyomBolotin@users.noreply.github.com>
2022-03-17 13:35:30 +0300

    Add files via upload

---
+++ WebUl/Controllers/BookController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Domain.Abstract;
+using Domain.Entities;
+
+namespace WebUI.Controllers
+{
+    public class BookController : Controller
+    {
+        private IBookRepository repository;
+        public BookController(IBookRepository repo)
+        {
+            repository = repo;
+        }
+        public ViewResult List()
+        {
+            return View(repository.Books);
+        }
+    }
+}
---
+++ WebUl/infrastructure/NinjectDependencyResplver.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Web.Mvc;
+using Moq;
+using Ninject;
+using Domain.Abstract;
+using Domain.Entities;
+
+namespace WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+
+        private void AddBindings()
+        {
+            Mock<IBookRepository> mock = new Mock<IBookRepository>();
+            mock.Setup(m => m.Books).Returns(new List<Book>
+    {
+        new Book { Name = "Тихий Дон",Author = "Шолохов Михаил", Price = 456 },
+        new Book { Name = "Евгений Онегин",Author = "Пушкин Александр Сергеевич", Price = 576 },
+       
+    });
+            kernel.Bind<IBookRepository>().ToConstant(mock.Object);
+
+        }
+    }
+}
